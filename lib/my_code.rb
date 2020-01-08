0c8099c6d1cce0f45a919526c6dd72e74949c57e
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-08 12:26:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,5 +50,12 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  
+  i = 0 
+  while i < source_array.length
+    if !source_array[i]
+      return false
+    end 
+    i++
+  end
+  true 
 end
