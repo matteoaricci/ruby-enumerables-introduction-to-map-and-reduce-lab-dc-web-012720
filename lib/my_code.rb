0263c340a2fd501b888d6b9114c7fbc37cd48e84
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-08 12:08:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,18 +1,18 @@
 # My Code here....
 def map_to_negativize(source_array)
-  source_array.map { |n| n * -1 }
+  
 end
 
 def map_to_no_change(source_array)
-  source_array.map { |n| n }
+  
 end 
 
 def map_to_double(source_array)
-  source_array.map { |n| n * 2 }
+  
 end 
 
 def map_to_square(source_array)
-  source_array.map { |n| n ** 2 }
+  
 end 
 
 def reduce_to_total(source_array, starting_point)
