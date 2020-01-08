b43d75ff07286a024269040a5bfed5ea45bb623e
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-08 12:11:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,17 +9,17 @@ def map_to_negativize(source_array)
 puts arr
 end
 
-def map_to_no_change(source_array)
+# def map_to_no_change(source_array)
   
-end 
+# end 
 
-def map_to_double(source_array)
+# def map_to_double(source_array)
   
-end 
+# end 
 
-def map_to_square(source_array)
+# def map_to_square(source_array)
   
-end 
+# end 
 
-def reduce_to_total(source_array, starting_point)
+# def reduce_to_total(source_array, starting_point)
   
