f15713fcbbf52ee679939c06305694cc2bb04b72
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-08 12:32:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,72 @@
 # My Code here....
+def map_to_negativize(source_array)
+  arr = []
+  i = 0 
+   while i < source_array.length 
+   arr[i] = source_array[i] * -1
+   i += 1 
+  end 
+ arr
+end
+
+def map_to_no_change(source_array)
+  arr = []
+  i = 0 
+   while i < source_array.length 
+   arr[i] = source_array[i]
+   i += 1 
+  end 
+ arr
+end 
+
+def map_to_double(source_array)
+  arr = []
+  i = 0 
+   while i < source_array.length 
+   arr[i] = source_array[i] * 2
+   i += 1 
+  end 
+ arr
+end 
+
+def map_to_square(source_array)
+  arr = []
+  i = 0 
+   while i < source_array.length 
+   arr[i] = source_array[i] ** 2
+   i += 1 
+  end 
+ arr  
+ end 
+
+def reduce_to_total(source_array, starting_point = 0)
+  i = 0 
+  total = starting_point
+    while i < source_array.length 
+    total += source_array[i]
+    i += 1 
+  end
+ total
+end
+
+def reduce_to_all_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    if !source_array[i]
+      return false
+    end   
+    i += 1
+  end
+  return true 
+end
+
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do 
+      if source_array[i]
+        return true
+      end   
+      i += 1
+    end
+    return false 
+end
