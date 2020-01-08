2f39b24893cbbb1b51a5154a70ad68bb8927f565
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-08 12:13:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,13 +19,25 @@ def map_to_no_change(source_array)
  arr
 end 
 
-# def map_to_double(source_array)
-  
+def map_to_double(source_array)
+  arr = []
+  i = 0 
+   while i < source_array.length 
+   arr[i] = source_array[i] * 2
+   i += 1 
+  end 
+ arr
 # end 
 
-# def map_to_square(source_array)
-  
-# end 
+def map_to_square(source_array)
+  arr = []
+  i = 0 
+   while i < source_array.length 
+   arr[i] = source_array[i] ** 2
+   i += 1 
+  end 
+ arr  
+ end 
 
 # def reduce_to_total(source_array, starting_point)
   
