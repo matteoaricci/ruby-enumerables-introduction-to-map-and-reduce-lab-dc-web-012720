c147302b53a659bdd19d99720edea319afd1eeb6
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-08 12:12:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,9 +9,15 @@ def map_to_negativize(source_array)
  arr
 end
 
-# def map_to_no_change(source_array)
-  
-# end 
+def map_to_no_change(source_array)
+  arr = []
+  i = 0 
+   while i < source_array.length 
+   arr[i] = source_array[i]
+   i += 1 
+  end 
+ arr
+end 
 
 # def map_to_double(source_array)
   
