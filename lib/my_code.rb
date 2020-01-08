32fe9727ecd9804567f7824310f99f4760f54cef
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-08 12:05:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,10 @@ def map_to_no_change(source_array)
   source_array.map { |n| n }
 end 
 
+def map_to_double(source_array)
+  source_array.map { |n| n * 2 }
+end 
+
+def map_to_square(source_array)
+  source_array.map { |n| n ** 2 }
+end 
